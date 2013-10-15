c63b1c6cc9269b5ea4c2a040839b86ed3a8db2d5
Andreas Nüßlein <andreas@nuessle.in>
2013-10-15 16:44:01 +0200

    overhaul. speed up. clarity down.
    rewrite of the whole thing..
    
    sadly readability went south,
    but speed went up.
    
    mysql-import seems to be especially happy when each INSERT INTO-line is
    about 800k long.

--- mwdum.py
+++ mwdum.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys,re, copy
+import sys
 from random import random
 from dateutil.parser import parse
 from datetime import datetime
-
 
 try:
     import xml.etree.cElementTree as ET
@@ -14,8 +13,6 @@ except ImportError:
 
 uprint = lambda text: sys.stdout.buffer.write((text+'\n').encode('utf-8'))
 #uprint = lambda x: "" if x else ""
-
-
 
 wiki_namespaces = ["Talk",
     "User","User_talk","Wikipedia","Wikipedia_talk",
@@ -27,182 +24,203 @@ wiki_namespaces = ["Talk",
     "TimedText","TimedText_talk",
     "Module","Module_talk"]
 
-
 def escapeSQL(string):
-    newstr = ""
     newstr = string.replace('\\','\\\\')\
             .replace('"','\\"')\
             .replace('\'','\\\'')\
             .replace('\u0000','\\0')\
             .replace('\n','\\n')\
             .replace('\r','\\r')\
-            .replace('\u001a','\\Z')\
-
+            .replace('\u001a','\\Z')
     return "'%s'" % newstr
-
-""" Models for the XML-Structure """
-class Contributor:
-    pass
-
-class Rev:
-    pass
-
-class Page:
-    def __init__(self):
-        self.revs = []
-    def __repr__(self):
-        ret = ["%s: %s" %(k,v) for k,v in
-                sorted(self.__dict__.items(),
-                    key=lambda x: x[0])
-                if not k=='revs' ]
-        return(", ".join(ret))
-
-
-class PageGenerator:
-    def __init__(self, output_function):
-        self.stack = []
-        self.output_function = output_function
-    def __repr__(self):
-        return("Page: %s\nRev: %s" %(self.page,self.revision))
-
-    def newE(self, elem):
-        if elem == 'page':
-            e = Page()
-        elif elem == 'revision':
-            e = Rev()
-        elif elem == 'contributor':
-            e = Contributor()
-        setattr(self,elem,e)
-        self.stack.append(elem)
-
-    def addAttr(self, attr, value):
-        try:
-            elem = getattr(self, self.stack[-1])
-            setattr(elem, attr, value)
-        except:
-            pass
-
-    def pop(self):
-        elem = self.stack.pop()
-        if elem == 'contributor':
-            self.revision.contributor = self.contributor
-            del(self.contributor)
-        elif elem == 'revision':
-            self.page.revs.append(self.revision)
-            del(self.revision)
-        elif elem == 'page':
-            self.output_function(self.page)
-            del(self.page)
-
 
 
 class MWDump:
-    def __init__(self, input_file):
+    def __init__(self, input_file, output_function):
         self.ETip = ET.iterparse(input_file,events=("start","end"))
         first = next(self.ETip)[1]
-        self.ns = first.tag[:-len('mediawiki')]
-        self.els = []
+        self.xmlns = first.tag[:-len('mediawiki')]
+        self.stack = []
 
-    def output_mysql(self, page):
-
-        ## revs ##
-        text = []
-
-        revarray = []
-
-        latest_rev = sorted(page.revs, key=lambda x: x.timestamp)[-1]
-
-        for rev in page.revs:
-            if not rev.text:
-                rev.text = ''
-            text += [(rev.id, escapeSQL(rev.text))]
-
-            try:
-                comment = escapeSQL(rev.comment)
-            except:
-                comment = "''"
-
-            minor = 1 if 'minor' in rev.__dict__.keys() else 0
-            deleted = 1 if 'deleted' in rev.__dict__.keys() else 0
-            try:
-                user = rev.contributor.id
-                user_text = escapeSQL(rev.contributor.username)
-            except:
-                user = 0
-                try:
-                    user_text = "'%s'" % (rev.contributor.ip)
-                except:
-                    user_text = "''"
-
-            if 'deleted' in rev.__dict__.keys():
-                import ipdb;ipdb.set_trace()
-                """ LOOK THERES A DELETED FLAG"""
-
-            timestamp = parse(rev.timestamp).strftime('%Y%m%d%H%M%S')  # obviously works better but is slower
-
-            revarray += ["(%s, %s, %s, %s, %s, %s, '%s', %s, %s)" %(
-                    rev.id, page.id, rev.id, comment, user, user_text,
-                    timestamp, minor, deleted ) ]
-
-
-        ## page ##
-        touched = datetime.now().strftime('%Y%m%d%H%M%S')
-        try:
-            title = escapeSQL(page.title.replace(" ","_"))
-#            if title[0:11] == "'User_talk:":
-            splitsies = title.split(":",1)
-            if splitsies[0] in ["'"+x for x in wiki_namespaces]:
-#                sys.stderr.write(str(splitsies)+"\n")
-                title = "'"+splitsies[1]
-        except:
-            title = ""
-        redirect = 1 if 'redirect' in page.__dict__.keys() else 0
-        ins = "page_id,page_namespace,page_title,page_is_redirect,page_random,page_touched,page_latest,page_len"
-        val = "%s,%s,%s,%s,%s,%s,%s,%s" %(
-                page.id, page.ns, title, redirect,
-                random(), touched, latest_rev.id, len(latest_rev.text))
-
-        try:
-            v = page.restrictions
-            ins += ',page_restrictions'
-            val += ",'%s'" %v
-            import ipdb;ipdb.set_trace()
-            """ LETS CHECK OUT RESTRICTIONS """
-        except:
-            pass
-
-        sql = "INSERT INTO page(%s) VALUES (%s);" %(ins,val)
-        uprint(sql)
-
-        ### text ###
-        val = ",".join(["(%s,%s,'utf-8')" %(tid,txt) for tid,txt in text])
-        sql = "INSERT INTO text(old_id,old_text,old_flags) VALUES %s;" %(val)
-        uprint(sql)
-
-        #### revisions ###
-        ins = "INSERT INTO revision (rev_id,rev_page,rev_text_id,rev_comment,rev_user,rev_user_text,rev_timestamp,rev_minor_edit,rev_deleted) VALUES %s;"
-        val = ",".join(revarray)
-        sql = ins % (val)
-        uprint(sql)
-
-
-
+        self.page = {}
+        self.rev = {}
+        self.latest_rev = {}
+        self.output_function = output_function()
 
     def run(self):
-        pg = PageGenerator(self.output_mysql)
         for ev, el in self.ETip:
-            cleanTag = el.tag[len(self.ns):]
+            cleanTag = el.tag[len(self.xmlns):]
 
-            if cleanTag in ['page','revision','contributor']:
-                if ev == 'start':
-                    pg.newE(cleanTag)
-                if ev == 'end':
-                    pg.pop()
-                el.clear()
-            else:
-                pg.addAttr(cleanTag,el.text)
+            if ev == 'start':
+                if cleanTag == 'contributor':
+                    self.rev['user'] = 0
+                    self.rev['user_text'] = "''"
+                    self.stack.append('contributor')
+                elif cleanTag == 'revision':
+                    self.stack.append('revision')
+                elif cleanTag == 'page':
+                    self.stack.append('page')
 
-            self.els.append(el)
+            try:
+                level = self.stack[-1]
+            except:
+                level = None
+
+            if level == 'contributor':
+                if cleanTag == 'contributor' and ev == 'end':
+                    self.stack.pop()
+                elif el.text:
+                    if cleanTag == 'id':
+                        self.rev['user'] = el.text
+                    elif cleanTag in ['username','ip']:
+                        self.rev['user_text'] = el.text
+
+            elif level == 'revision':
+                if cleanTag in ['minor', 'deleted']:
+                    self.rev[cleanTag] = 1
+                elif el.text and cleanTag in ['id', 'parentid', 'timestamp',
+                        'comment', 'text', 'parentid', 'sha1', 'model', 'format']:
+                    self.rev[cleanTag] = el.text
+
+                elif ev == 'end' and cleanTag == 'revision':
+
+                    # prepare data
+                    if not 'comment' in self.rev:
+                        self.rev['comment'] = ""
+                    if not 'text' in self.rev:
+                        self.rev['text'] = ""
+                    if not 'parentid' in self.rev:
+                        self.rev['parentid'] = 'NULL'
+
+                    self.rev['sha1'] = "'%s'" %(self.rev['sha1']) if 'sha1' in self.rev else "''"
+                    self.rev['model'] = "'%s'" %(self.rev['model']) if 'model' in self.rev else "NULL"
+                    self.rev['format'] = "'%s'" %(self.rev['format']) if 'format' in self.rev else "NULL"
+                    self.rev['page'] = self.page['id']
+                    self.rev['minor'] = 1 if 'minor' in self.rev else 0
+                    self.rev['deleted'] = 0 if 'deleted' in self.rev else 0
+                    self.rev['timestamp'] = parse(self.rev['timestamp']).strftime('%Y%m%d%H%M%S')
+
+                    # latest rev
+                    if 'timestamp' not in self.latest_rev or self.latest_rev['timestamp'] < self.rev['timestamp']:
+                        self.latest_rev = self.rev
+
+                    self.output_function.run('revision', self.rev)
+
+                    self.rev = {}
+                    self.stack.pop()
+
+
+            elif level == 'page':
+                if cleanTag in ['redirect']:
+                    self.page[cleanTag] = 1
+                elif el.text and cleanTag in ['id','ns','title','restrictions']:
+                    if cleanTag == 'title':
+                        try:
+                            title = escapeSQL(el.text.replace(" ","_"))
+                            splitsies = title.split(":",1)
+                            if splitsies[0] in ["'"+x for x in wiki_namespaces]:
+                                title = "'"+splitsies[1]
+                        except:
+                            title = ""
+                        self.page['title'] = title
+
+                    else:
+                        self.page[cleanTag] = el.text
+
+                elif ev == 'end' and cleanTag == 'page':
+                    self.page['random'] = random()
+                    self.page['touched'] = datetime.now().strftime('%Y%m%d%H%M%S')
+                    self.page['latest_rev'] = self.latest_rev['id']
+                    self.page['latest_rev_len'] = len(self.latest_rev['text'])
+
+                    self.page['redirect'] = 1 if 'redirect' in self.page else 0
+                    if not 'restrictions' in self.page:
+                        self.page['restrictions'] = ""
+
+                    self.output_function.run('page', self.page)
+                    self.stack.pop()
+                    self.page = {}
+                    self.latest_rev = {}
+
+            el.clear()
+        self.output_function.end()
+
+
+class MySQL_Output:
+    class MyPrint:
+        def __init__(self):
+            self.limit = 80 * 1024 * 1024
+            self.size = 0
+            uprint('BEGIN;')
+        def do(self, text):
+#            self.size += len(text)
+#            if self.size > self.limit:
+#                uprint('COMMIT;\nBEGIN;')
+#                self.size = 0
+            uprint(text)
+
+
+    class SQLInsertLineBuffer:
+        limit = 800 * 1024
+        def __init__(self, print_function, sql_statement):
+            self.statement = sql_statement
+            self.size = 0
+            self.array = []
+            self.print_function = print_function
+
+        def add(self, mydata):
+            self.size += len(mydata)
+            if self.size > self.limit:
+                self.doprint()
+            self.array += [mydata]
+
+        def doprint(self):
+            sql = self.statement % (",".join(self.array))
+            self.print_function(sql)
+
+            self.size = 0
+            self.array = []
+
+        def finish(self):
+            self.doprint()
+
+    def __init__(self):
+        myprint = self.MyPrint()
+        self.text = self.SQLInsertLineBuffer( myprint.do,
+            "INSERT INTO text (old_id,old_text,old_flags) VALUES %s;")
+        self.rev = self.SQLInsertLineBuffer( myprint.do,
+            "INSERT INTO revision (rev_id,rev_page,rev_text_id,rev_comment,rev_user,rev_user_text,rev_timestamp,rev_minor_edit,rev_deleted,rev_parent_id,rev_sha1,rev_content_model,rev_content_format) VALUES %s;")
+        self.page = self.SQLInsertLineBuffer( myprint.do,
+            "INSERT INTO page (page_id,page_namespace,page_title,page_is_redirect,page_random,page_touched,page_latest,page_len,page_restrictions) VALUES %s;")
+
+
+    def run(self, mytype, mydata):
+        if mytype == 'page':
+            page = mydata
+            self.page.add("(%s,%s,%s,%s,%s,%s,%s,%s,%s)" %(
+                page['id'], page['ns'], page['title'], page['redirect'],
+                page['random'], page['touched'],
+                page['latest_rev'], page['latest_rev_len'],
+                escapeSQL(page['restrictions']))
+                )
+
+        elif mytype == 'revision':
+            rev = mydata
+            self.text.add( "(%s,%s,'utf-8')" % (
+                    rev['id'], escapeSQL(rev['text']) )
+                )
+            self.rev.add( "(%s,%s,%s,%s,%s,%s,'%s',%s,%s,%s,%s,%s,%s)" % (
+                rev['id'], rev['page'], rev['id'], escapeSQL(rev['comment']),
+                rev['user'], escapeSQL(rev['user_text']), rev['timestamp'],
+                rev['minor'], rev['deleted'], rev['parentid'], rev['sha1'],
+                rev['model'], rev['format'])
+                )
+
+    def end(self):
+        self.page.finish()
+        self.text.finish()
+        self.rev.finish()
+        uprint('COMMIT;')
 
 
 if len(sys.argv) > 1:
@@ -211,5 +229,6 @@ else:
     filename = sys.stdin.readline()[:-1]
 
 
-mwd = MWDump(filename)
+mwd = MWDump(filename, MySQL_Output)
 mwd.run()
+
